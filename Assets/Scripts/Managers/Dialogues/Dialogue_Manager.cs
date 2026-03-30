c125352e08676c9dd64d8147f9867a2f865a003e
PrinceYuno1205 <alecmiguel.fortes36@gmail.com>
2026-03-30 19:25:09 +0800

    push

--- Assets/Scripts/Managers/Dialogues/Dialogue_Manager.cs
+++ Assets/Scripts/Managers/Dialogues/Dialogue_Manager.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Dialogue_Manager : MonoBehaviour
 {
-    // public Image _actorAvatar;
-    // public TextMeshProGUI _actorName;
-    // public TextMeshProGUI _messageText;
-    // public Transform
+    public Image _actorAvatar;
+    public TMP_Text _actorName;
+    public TMP_Text _messageText;
+    public GameObject _dialogueBox;
+    
+    private Animator _dialogueBoxAnimator;
+
+    private void Start()
+    {
+        _dialogueBoxAnimator = GetComponent<Animator>();
+    }
+
+    public void StartDialogue(string _dialogueID)
+    {
+        _dialogueBoxAnimator.SetBool("Active", true);
+    }
+
+    public void EndtDialogue(string _dialogueID)
+    {
+        _dialogueBoxAnimator.SetBool("Active", false);
+    }
 }
 
 
