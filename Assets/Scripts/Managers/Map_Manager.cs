64cb3cc27a2753006727dd5830cde6aa1d993c52
Chunchunmaru <thomasgabinete@gmail.com>
2026-05-08 10:55:16 +0800

    fix: map transition

--- Assets/Scripts/Managers/Map_Manager.cs
+++ Assets/Scripts/Managers/Map_Manager.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class Map_Manager : MonoBehaviour
 {
-    [SerializeField] private string _stageName;
+    [SerializeField] private int _stageNum;
+    [SerializeField] private Scene_Manager _sceneManager;
 
     public virtual void Interact(Map_Player _player)
     {
-        SceneManager.LoadScene(_stageName);
+        // SceneManager.LoadScene(_stageName);
+        _sceneManager.FadeToScene(_stageNum);
     }
 
     public virtual void InteractAlternate(Map_Player _player)
