e8e0340809c0e6a6a1aeedd5b6383741ebbdd90a
PrinceYuno1205 <alecmiguel.fortes36@gmail.com>
2026-05-29 11:50:21 +0800

    base products to main data

--- Assets/Scripts/Data/GameInventory_Data.cs
+++ Assets/Scripts/Data/GameInventory_Data.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameInventory_Data", menuName = "Scriptable Objects/GameInventory_Data")]
 public class GameInventory_Data : ScriptableObject
 {
-    [Header("Inventory")]
+    [Header("Final Products Inventory")]
     public List<Item_Data> items = new List<Item_Data>();
     public int _keyChains = 0;
     public int _toteBags = 0;
     public int _buttonPins = 0;
     public int _figurines = 0;
+
+    [Header("Extra Materials Inventory")]
+    public int _scrapMetal = 0;
+    public int _scrapWood = 0;
+    public int _scrapGlass = 0;
+    public int _scrapPaint = 0;
+    public int _scrapFabric = 0;
 
     [Header("Currency")]
     public int _playerMoney = 0;
