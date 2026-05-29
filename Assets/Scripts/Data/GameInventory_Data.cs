54a119819f5361e41161c385730149bf5009b2e2
PrinceYuno1205 <alecmiguel.fortes36@gmail.com>
2026-05-29 11:47:35 +0800

    player money in main data

--- Assets/Scripts/Data/GameInventory_Data.cs
+++ Assets/Scripts/Data/GameInventory_Data.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameInventory_Data", menuName = "Scriptable Objects/GameInventory_Data")]
 public class GameInventory_Data : ScriptableObject
 {
+    [Header("Inventory")]
     public List<Item_Data> items = new List<Item_Data>();
     public int _keyChains = 0;
     public int _toteBags = 0;
     public int _buttonPins = 0;
     public int _figurines = 0;
+
+    [Header("Currency")]
+    public int _playerMoney = 0;
+
 
     public void ClearInventory()
     {
