782aa8863840eea6fe2300f4e8fdbf349ae41304
Chunchunmaru <thomasgabinete@gmail.com>
2026-05-29 13:10:06 +0800

    refactor: removed unnecessary inventory script lines

--- .history/Assets/Scripts/Data/GameInventory_Data_20260520152108.cs
+++ .history/Assets/Scripts/Data/GameInventory_Data_20260520152108.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class GameInventory_Data : ScriptableObject
 {
     public List<Item_Data> items = new List<Item_Data>();
+    public int _keyChains = 0;
+    public int _toteBags = 0;
+    public int _buttonPins = 0;
+    public int _figurines = 0;
 
     public void ClearInventory()
     {
