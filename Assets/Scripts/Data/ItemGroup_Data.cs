93267781d42f8672a0decf196c846116c2cc5b02
Chunchunmaru <thomasgabinete@gmail.com>
2026-06-09 10:46:55 +0800

    bago na naman

--- Assets/Scripts/Data/ItemGroup_Data.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemGroup_Data", menuName = "Scriptable Objects/ItemGroup_Data")]
-public class ItemGroup_Data : ScriptableObject
-{
-    public Transform _itemGroupPrefab;
-}
